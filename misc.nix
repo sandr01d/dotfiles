db9df7ba0fe6ee8a4715e2557d9cffb421571667
sandroid <sandroid@posteo.net>
2025-07-31 23:20:14 +0200

    add aliases for eza and erdtree

--- misc.nix
+++ misc.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    eza # ls replacement
+    erdtree
+  ];
+
   home.sessionVariables = {
     # Colored manpages using bat
     MANPAGER = "sh -c 'col -bx | bat -l man -p'";
@@ -11,5 +16,7 @@
     df = "df -h";
     free = "free -m";
     q = "exit";
-    }; 
+    ll = "eza --icons=always -l";
+    e = "erd --human --icons --layout=inverted";
+  }; 
 }
